76e02fda01b889a7c6f7b4a843fc25bb6384c572
Angelo Mantellini <amantellini@gmail.com>
2019-05-30 00:56:16 -0700

    Add hicn formula

---
+++ Formula/hicn.rb
@@ -0,0 +1,20 @@
+class Hicn < Formula
+  desc "This formula contains the libs and the apps of hICN stack"
+  homepage "https://wiki.fd.io/view/HICN"
+  url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
+  version "19.04-47-g088daca"
+
+  depends_on "cmake" => :build
+  depends_on "libparc"
+  depends_on "curl"
+  depends_on "asio"
+
+  def install
+    system "cmake", ".", "-DBUILD_APPS=ON",*std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "true"
+  end
+end
